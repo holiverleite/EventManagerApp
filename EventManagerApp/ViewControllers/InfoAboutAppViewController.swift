049545912474138abd13ffa137fbf6fa0b08136b
Haroldo Leite <holiverleite@gmail.com>
2019-08-28 17:53:12 -0300

    -Implemented an About screen;

--- EventManagerApp/ViewControllers/InfoAboutAppViewController.swift
+++ EventManagerApp/ViewControllers/InfoAboutAppViewController.swift
@@ -10,12 +10,46 @@ import UIKit
 
 class InfoAboutAppViewController: UIViewController {
 
-    @IBOutlet weak var nameAppLabel: UILabel!
-    @IBOutlet weak var logoAppImageView: UIImageView!
-    @IBOutlet weak var developedByLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameAppLabel: UILabel! {
+        didSet {
+            self.nameAppLabel.textColor = UIColor.greenLogo
+        }
+    }
     
-    @IBOutlet weak var aboutProjectLAbel: UILabel!
+    @IBOutlet weak var logoAppImageView: UIImageView! {
+        didSet {
+            self.logoAppImageView.image = UIImage(named: "LaunchImage")
+        }
+    }
+    
+    @IBOutlet weak var developedByLabel: UILabel! {
+        didSet {
+            self.developedByLabel.textColor = UIColor.greenLogo
+            self.developedByLabel.text = "Desenvolvido por: \n Haroldo Oliveira de Almeida Leite \n holiverleite@gmail.com"
+        }
+    }
+    
+    @IBOutlet weak var subjectLabel: UILabel! {
+        didSet {
+            self.subjectLabel.textColor = UIColor.greenLogo
+            self.subjectLabel.text = "TÓPICOS EM DESENVOLVIMENTO DE PROGRAMAS MÓVEIS 2 \n (TDMP3)"
+        }
+    }
+    
+    @IBOutlet weak var descriptionLabel: UILabel! {
+        didSet {
+            self.descriptionLabel.textColor = UIColor.greenLogo
+            self.descriptionLabel.text = " Prof. Dr. Fernando Vernal Salina"
+        }
+    }
+    
+    @IBOutlet weak var aboutProjectLAbel: UILabel! {
+        didSet {
+            self.aboutProjectLAbel.textColor = UIColor.greenLogo
+            self.aboutProjectLAbel.text = "IFSP - 2019"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
